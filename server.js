472ccd176ede69669c831ade276185f7e60b7dc0
heose <goddnrl@gmail.com>
2018-10-10 12:36:27 +0900

    q

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
+const cookieParser = require('cookie-parser');
 
 app.prepare()
   .then(() => {
     const server = express();
-
+    server.use(cookieParser());
+    server.use((req, res, next) => {
+      const baseUserName = req.cookies.user_name;
+      console.log('user name:', Buffer.from(encodeURIComponent(baseUserName), 'base64').toString('utf-8'));
+      next();
+    });
     server.get('/a', (req, res) => {
       return app.render(req, res, '/about', req.query);
     });
@@ -27,6 +33,7 @@ app.prepare()
     server.get('*', (req, res) => {
       return handle(req, res);
     });
+
 
     server.listen(port, (err) => {
       if (err) throw err;
