bcc950020e12209df30a98c3d34ed9861b731026
heose <goddnrl@gmail.com>
2018-10-16 19:29:46 +0900

    a

--- apis/company-api.test.js
+++ apis/company-api.test.js
@@ -5,7 +5,7 @@ jest.mock('./company-api');
 describe('should fetch companies', () => {
   it('should be ok', async () => {
     const companyApi = new CompanyApi();
-    await companyApi.fetchCompanies({}).then(data =>{
+    await companyApi.fetchCompanies({}).then(({data}) =>{
       expect(data.result.list.length).toEqual(20);
     })
   });
