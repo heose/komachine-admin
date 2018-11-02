4ac70721aaf2c5265e7c58e120ae2133219cc1ac
heose <goddnrl@gmail.com>
2018-11-02 09:38:24 +0900

    a

---
+++ components/CompanyListItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CompanyListItem = () => {
+  return (
+    <div />
+  )
+}
+
+CompanyListItem.PropTypes = {
+
+}
+
+CompanyListItem.defaultProps = {
+
+}
+
+export default CompanyListItem;
--- stores/company-store.js
+++ stores/company-store.js
@@ -1,16 +1,23 @@
-import {action, observable, computed} from 'mobx';
+import { action, observable, computed } from 'mobx';
 import CompanyApi from '../apis/company-api';
 
-
 export default class CompanyStore {
-  @observable table = {};
-  @observable list = [];
-  @observable state = '';
-  @observable page = '1';
-  @observable hasPrev = false;
-  @observable hasNext = false;
-  @observable isActive = null;
-  @observable hasRelation = null;
+  @observable
+  table = {};
+  @observable
+  list = [];
+  @observable
+  state = '';
+  @observable
+  page = '1';
+  @observable
+  hasPrev = false;
+  @observable
+  hasNext = false;
+  @observable
+  isActive = null;
+  @observable
+  hasRelation = null;
 
   constructor(initialState, api) {
     const { companyStore } = initialState || {};
@@ -30,14 +37,15 @@ export default class CompanyStore {
   @action
   async fetchCompanies(query) {
     this.state = 'pending';
-    await this.api.fetchCompanies(query)
-      .then(({data}) => this.fetchCompaniesSuccess(data))
-      .catch(this.fetchCompaniesError)
+    await this.api
+      .fetchCompanies(query)
+      .then(({ data }) => this.fetchCompaniesSuccess(data))
+      .catch(this.fetchCompaniesError);
   }
 
   @action.bound
   fetchCompaniesSuccess(data) {
-    this.table = {...this.table, ...data.result.table};
+    this.table = { ...this.table, ...data.result.table };
     this.list = data.result.list;
     this.state = 'complete';
     this.page = data.result.page;
@@ -54,8 +62,8 @@ export default class CompanyStore {
 
   @computed
   get queryMap() {
-    const {isActive, hasRelation, page} = this;
-    return {isActive, hasRelation, page};
+    const { isActive, hasRelation, page } = this;
+    return { isActive, hasRelation, page };
   }
 
   @computed
