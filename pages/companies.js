d6dcc33274bf8c4714cbb05d007dc34a62894d88
heose <goddnrl@gmail.com>
2018-11-04 00:14:28 +0900

    a

--- pages/companies.js
+++ pages/companies.js
@@ -1,29 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CompanyList from 'components/CompanyList';
 import withLayout from '../lib/with-layout';
 import DefaultLayout from '../layouts/DefaultLayout';
 
-
 class Companies extends React.Component {
-
-  static async getInitialProps ({req, query, rootStore}) {
-    const isServer = !!req;
+  static async getInitialProps({ query, rootStore }) {
     const { companyStore } = rootStore;
     await companyStore.fetchCompanies(query);
-    return { query }
-  }
-
-  constructor(props) {
-    super(props);
+    return { query };
   }
 
   render() {
+    const { query } = this.props;
     return (
       <div>
-        <CompanyList query={this.props.query} />
+        <CompanyList query={query} />
       </div>
     );
   }
 }
+
+Companies.propTypes = {
+  query: PropTypes.string.isRequired,
+};
 
 export default withLayout(DefaultLayout)(Companies);
