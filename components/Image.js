214e6d4eb2e4419ea1a54acfb9e674a2cb224cef
heose <goddnrl@gmail.com>
2018-12-03 19:23:41 +0900

    a

--- components/Image.js
+++ components/Image.js
@@ -1,86 +1,110 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 export default class Image extends Component {
   constructor(props) {
     super(props);
     this.tinyRef = React.createRef();
-    this.tinyRef1 = React.createRef();
     this.originRef = React.createRef();
-    this.handleLoad = this.handleLoad.bind(this);
+    this.handleLoaded = this.handleLoaded.bind(this);
     this.handleError = this.handleError.bind(this);
   }
 
+  state = {
+    tinyState: 'loading',
+    originState: 'loading',
+  };
+
   componentDidMount() {
-    console.log('componentDidMount');
-    console.log(this.tinyRef.current.complete);
-    console.log(this.tinyRef.current.naturalWidth);
-    console.log(this.tinyRef1.current.naturalWidth);
-    // const src = 'https://cdn.komachine.com/media/company-logo/cascor-768_vhxboc.JPG';
-    // const tinyImg = new Image();
-    // tinyImg.addEventListener('load', e => {
-    //   console.log('Image object load');
-    //   console.log(e);
-    // });
-    // tinyImg.addEventListener('error', e => {
-    //   console.log('error');
-    //   console.log(e);
-    // });
-    // tinyImg.src = src;
+    if (this.tinyRef.current.complete) {
+      if (this.tinyRef.current.naturalWidth) {
+        this.setState(() => ({ tinyState: 'complete' }));
+      } else {
+        this.setState(() => ({ tinyState: 'failure' }));
+      }
+    }
   }
 
-  handleLoad() {
-    console.log('img tag load');
+  handleLoaded(type) {
+    this.setState(() => ({ [type]: 'complete' }));
   }
 
-  handleError() {
-    console.log('error');
+  handleError(type) {
+    this.setState(() => ({ [type]: 'failure' }));
   }
 
   render() {
+    const { tinyState, originState } = this.state;
+    const { height, src } = this.props;
+    let origin = null;
+    if (tinyState !== 'loading') {
+      origin = (
+        <Origin
+          as="img"
+          ref={this.originRef}
+          src="https://cdn.komachine.com/media/2013-Porsche-Cayenne-Gts-1920x2560.jpeg"
+          alt=""
+          onLoad={() => this.handleLoaded('originState')}
+          onError={this.handleError}
+          originState={originState}
+        />
+      );
+    }
     return (
-      <Wrapper>
-        <Empty />
+      <Wrapper height={height}>
+        <Init tinyState={tinyState} height={height} />
         <Tiny
+          as="img"
           ref={this.tinyRef}
-          src="https://cdn.komachine.com/media/company-logo/cascor-768_vhxboc_tiny11.jpg"
+          src="https://cdn.komachine.com/media/2013-Porsche-Cayenne-Gts-1920x2560_tiny.jpg"
           alt=""
-          onLoad={this.handleLoad}
-          onError={() => console.log('error')}
+          onLoad={() => this.handleLoaded('tinyState')}
+          onError={() => this.handleError('tinyState')}
+          tinyState={tinyState}
+          originState={originState}
         />
-        <Tiny
-          ref={this.tinyRef1}
-          src="https://cdn.komachine.com/media/company-logo/cascor-768_vhxboc_tiny.jpg"
-          alt=""
-          onLoad={this.handleLoad}
-          onError={() => console.log('error')}
-        />
-        {/* <Origin ref={this.originRef} src="https://cdn.komachine.com/media/company-logo/cascor-768_vhxboc.JPG" alt="" /> */}
+        {origin}
       </Wrapper>
     );
   }
 }
 
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+};
+
 const Wrapper = styled.div`
-  height: 400px;
+  height: ${props => props.height};
+  width: auto;
+  position: relative;
+  padding: 2px;
 `;
 
-const Empty = styled.div`
-  height: 100px;
-  background-color: gray;
-  width: 300px;
+const Default = styled.div`
+  height: 100%;
+  background-color: lightgray;
+  width: auto;
   margin-bottom: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
 
-const Tiny = styled.img`
-  height: 100px;
-  display: block;
-  margin-bottom: 20px;
-  filter: blur(10px);
+const Init = styled(Default)`
+  height: ${props => props.height};
+  min-width: 48px;
 `;
 
-const Origin = styled.img`
-  height: 100px;
-  display: block;
+const Tiny = styled(Default)`
+  opacity: ${props => (props.tinyState === 'complete' ? 1 : 0)};
+  transition: opacity 0.3s;
+  filter: blur(5px);
+`;
+
+const Origin = styled(Default)`
+  opacity: ${props => (props.originState === 'complete' ? 1 : 0)};
+  transition: opacity 0.3s;
 `;
