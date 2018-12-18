401be6b425b07c3efdb8f8c7c090e05b4d7e2078
heose <goddnrl@gmail.com>
2018-12-18 21:52:50 +0900

    a

--- components/DnDFileUploader.js
+++ components/DnDFileUploader.js
@@ -1,86 +1,95 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import axios from 'axios';
 
-function DnDFileUploader(props) {
-  if (process.browser) {
-    console.log('adf');
-    // window.addEventListener('drop', e => e.preventDefault(), false);
-    // window.addEventListener('dragover', e => e.preventDefault(), false);
-    // window.addEventListener(
-    //   'dragenter',
-    //   e => {
-    //     e.preventDefault();
-    //     // e.dataTransfer.effectAllowed = 'none';
-    //     // e.dataTransfer.dropEffect = 'none';
-    //   },
-    //   false,
-    // );
+class DnDFileUploader extends React.Component {
+  state = {};
+  render() {
+    if (process.browser) {
+      window.addEventListener('dragover', e => e.preventDefault(), false);
+      window.addEventListener('drop', e => e.preventDefault(), false);
+    }
 
-    window.addEventListener(
-      'dragover',
-      e => {
-        e.dataTransfer.effectAllowed = 'none';
-        e.dataTransfer.dropEffect = 'none';
-        e.preventDefault();
-      },
-      false,
-    );
+    const handleFileChange = e => {
+      console.log(e.type);
+      console.log(e.target.files);
+    };
 
-    window.addEventListener(
-      'drop',
-      e => {
-        e.preventDefault();
-        e.dataTransfer.effectAllowed = 'none';
-        e.dataTransfer.dropEffect = 'none';
-      },
-      false,
+    const handleDragEnter = e => {
+      e.stopPropagation();
+      e.preventDefault();
+    };
+
+    const handleDrop = e => {
+      e.preventDefault();
+      console.log(e.type);
+      console.log(e.dataTransfer.items);
+      console.log(typeof e.dataTransfer.files);
+    };
+
+    const handleFile = e => {
+      let file;
+      if (e.type === 'change') {
+        [file] = e.target.files;
+      } else if (e.type === 'drop') {
+        [file] = e.dataTransfer.files;
+      }
+      console.log(file);
+      const formData = new FormData();
+      formData.append('file', file);
+      const config = {
+        onUploadProgress: ProgressEvent => {
+          const percentCompleted = Math.round((ProgressEvent.loaded * 100) / ProgressEvent.total);
+          console.log(percentCompleted);
+        },
+      };
+      axios
+        .put('http://localhost:8000/en/test/upload/', formData, config)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    };
+    return (
+      <Div onDragEnter={handleDragEnter} onDrop={handleFile}>
+        <span>드래그 or </span>
+        <label htmlFor="dndfile">
+          <input type="file" id="dndfile" onChange={handleFile} />
+          <span>파일선택</span>
+        </label>
+      </Div>
     );
   }
-
-  const handleFileChange = e => {
-    console.log(e.target.files);
-  };
-  const handleDragEnter = e => {
-    console.log(e);
-    e.dataTransfer.dropEffect = 'copy';
-    e.preventDefault();
-  };
-  const handleDrop = e => {
-    e.preventDefault();
-    console.log(e);
-  };
-  return (
-    <Div onDragEnter={handleDragEnter} onDrop={handleDrop}>
-      <label htmlFor="dndfile">
-        <input type="file" id="dndfile" onChange={handleFileChange} />
-        파일선택
-      </label>
-    </Div>
-  );
 }
 
 DnDFileUploader.propTypes = {};
 
 const Div = styled.div`
-  width: 200px;
-  height: 100px;
-  background-color: gray;
-  input {
-    width: 0.1px;
-    height: 0.1px;
-    opacity: 0;
+  width: 130px;
+  height: 50px;
+  background-color: #fff;
+  border: 1px solid lightgray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column nowrap;
+  input[type='file'] {
+    position: absolute !important;
+    height: 1px;
+    width: 1px;
     overflow: hidden;
-    position: absolute;
-    z-index: -1;
+    clip: rect(1px, 1px, 1px, 1px);
   }
   label {
-    display: inline-block;
-    width: 100px;
-    height: 50px;
+    display: inline-flex;
+    width: 90px;
+    height: 20px;
+    color: #fff;
     background-color: lightgray;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
     &:hover {
-      background-color: skyblue;
+      background-color: gray;
     }
   }
 `;
