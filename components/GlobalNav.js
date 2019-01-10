dce412a7888cf4a461c5d191c8bb26b1a462593a
heose <goddnrl@gmail.com>
2019-01-10 22:40:32 +0900

    a

--- components/GlobalNav.js
+++ components/GlobalNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Link from 'components/Link';
 import GlobalNavItem from 'components/GlobalNavItem';
 import CompanySVG from '../lib/svg/company.svg';
 import CategorySVG from '../lib/svg/category.svg';
@@ -9,25 +10,19 @@ import TranslationSVG from '../lib/svg/translation.svg';
 import InquirySVG from '../lib/svg/inquiry.svg';
 
 function GlobalNav(props) {
-  return (
-    <Div>
-      <GlobalNavItem label="기업">
-        <CompanySVG />
-      </GlobalNavItem>
-      <GlobalNavItem label="카테고리">
-        <CategorySVG />
-      </GlobalNavItem>
-      <GlobalNavItem label="동의어">
-        <SynonymSVG />
-      </GlobalNavItem>
-      <GlobalNavItem label="번역">
-        <TranslationSVG />
-      </GlobalNavItem>
-      <GlobalNavItem label="문의">
-        <InquirySVG />
-      </GlobalNavItem>
-    </Div>
-  );
+  const menus = [
+    { href: '/companies', label: '기업', icon: <CompanySVG /> },
+    { href: '/categories', label: '카테고리', icon: <CategorySVG /> },
+    { href: '/companies', label: '동의어', icon: <SynonymSVG /> },
+    { href: '/companies', label: '번역', icon: <TranslationSVG /> },
+    { href: '/companies', label: '문의', icon: <InquirySVG /> },
+  ].map(item => (
+    <Link href={item.href}>
+      <GlobalNavItem label={item.label}>{item.icon}</GlobalNavItem>
+    </Link>
+  ));
+
+  return <Div>{menus}</Div>;
 }
 
 GlobalNav.propTypes = {};
