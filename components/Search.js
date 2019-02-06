4ad92f0349eb23b5ab1bb1592e39c796bcdee383
heose <goddnrl@gmail.com>
2019-02-07 02:04:22 +0900

    a

--- components/Search.js
+++ components/Search.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'next/router';
-import Link from '~/components/Link';
 import { recomposeQueryStr } from '~/lib/with-link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.input = React.createRef();
-  }
-  state = { keyword: '' };
+  state = { keyword: this.props.router.query.search };
+
   handleKeyUp = e => {
     if (e.key === 'Enter' || e.keyCode === 13) {
       const { router } = this.props;
-      console.log(router);
-      console.log(this.input.current.value);
-      const href = `?search=${this.input.current.value}`;
-      const omitKeys = this.input.current.value ? ['page'] : ['page', 'search'];
-      const query = recomposeQueryStr(router.query, href, omitKeys);
-
+      let query = `?search=${this.state.keyword}`;
+      const omitKeys = this.state.keyword ? ['page'] : ['page', 'search'];
+      query = recomposeQueryStr(router.query, query, omitKeys);
+      query = query.endsWith('?') ? '' : query;
       router.push(`${router.pathname}${query}`);
-    } else {
-      this.setState(() => ({ keyword: this.input.current.value }));
     }
+  };
+  handleChange = e => {
+    e.persist();
+    this.setState(() => ({ keyword: e.target.value }));
   };
   render() {
     return (
       <Div>
-        <Input type="text" ref={this.input} onKeyUp={this.handleKeyUp} />
-        <Link href={`?search=${this.state.keyword}`}>검색</Link>
+        <Icon>
+          <FontAwesomeIcon icon="search" fixedWidth />
+        </Icon>
+        <Input
+          type="text"
+          onKeyUp={this.handleKeyUp}
+          value={this.state.keyword}
+          onChange={this.handleChange}
+          placeholder="기업명"
+        />
       </Div>
     );
   }
@@ -36,8 +41,37 @@ class Search extends React.Component {
 
 const Div = styled.div`
   display: inline-block;
+  position: relative;
 `;
 
-const Input = styled.input``;
+const Icon = styled.span`
+  position: absolute;
+  z-index: 10;
+  width: 21px;
+  height: 21px;
+  display: flex;
+  font-size: 2rem;
+  justify-content: center;
+  align-items: center;
+  left: 15px;
+  top: 50%;
+  color: #707070;
+  transform: translateY(-50%);
+`;
+
+const Input = styled.input`
+  border: 1.05rem solid white;
+  border-top: 1.4rem solid white;
+  border-left: 4.5rem solid white;
+  /* border-right: 1rem solid white; */
+  /* border-bottom: 0.5rem solid white; */
+  font-size: 1.6rem;
+  outline: 0;
+  border-radius: 10px;
+  outline-style: none;
+  appearance: none;
+  position: relative;
+  color: #707070;
+`;
 
 export default withRouter(Search);
